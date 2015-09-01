0fc8c948aa0523ccb9440d8bd4f7631e2921c27b
patriciotella <patriciotella@gmail.com>
2015-09-01 17:42:55 -0300

    testing users behaviour

--- SuperGol/src/test/java/ar/edu/unq/model/UserTest.java
+++ SuperGol/src/test/java/ar/edu/unq/model/UserTest.java
@@ -1,7 +1,31 @@
 package ar.edu.unq.model;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 public class UserTest {
 
+    private User testUser;
 
+    @Before
+    public void setUp(){
+        testUser = new User("User");
+    }
+
+    @Test
+    public void itShouldCreateATeamWithNameTestTeam(){
+        testUser.createTeam("TestTeam");
+
+        assertEquals(testUser.getTeam().getName(),"TestTeam");
+    }
+
+    @Test
+    public void itShouldAsociateTheUserThatCreatesTheTeamWithIt(){
+        testUser.createTeam("My Team");
+
+        assertEquals(testUser.getTeam().getOwner(),testUser);
+    }
 }
