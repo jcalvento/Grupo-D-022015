3ec0b32dce574d60fa20ad1001a6d8033f4e032b
jcalvento <julian.calvento@10pines.com>
2015-09-01 13:33:24 -0300

    US 04: - Add tests and 2 points when the goalkeeper did not receive any goals.

--- SuperGol/src/main/java/ar.edu.unq.model/Match.java
+++ SuperGol/src/main/java/ar.edu.unq.model/Match.java
@@ -1,16 +1,24 @@
 package ar.edu.unq.model;
 
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Match {
 
     private int round;
     private Team local;
     private Team visitor;
+    private Map<Team, Integer> scorer;
 
     public Match (int aRound, Team aTeam, Team anotherTeam){
         round = aRound;
         local = aTeam;
         visitor = anotherTeam;
+        scorer = new HashMap<>();
+        scorer.put(local, 0);
+        scorer.put(visitor, 0);
     }
 
     public String getMatchFormat(){
@@ -30,10 +38,17 @@ public class Match {
     }
 
     public void addGoal(Team aTeam, Player aPlayer) throws Exception {
+        scorer.replace(aTeam, scorer.get(aTeam) + 1);
         aTeam.addGoalOf(aPlayer, this);
     }
 
     public Integer pointsOf(Team aTeam) {
-        return aTeam.pointsMadeIn(this);
+        Integer points = aTeam.pointsMadeIn(this);
+        Team rival = Arrays.asList(local, visitor).stream()
+                .filter(team -> !team.equals(aTeam)).findFirst().get();
+        Integer rivalGoals = scorer.get(rival);
+        if(rivalGoals.equals(0))
+            points += 2;
+        return points;
     }
 }
--- SuperGol/src/test/java/ar.edu.unq.model/MatchTest.java
+++ SuperGol/src/test/java/ar.edu.unq.model/MatchTest.java
@@ -3,6 +3,7 @@ package ar.edu.unq.model;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 
@@ -10,13 +11,21 @@ public class MatchTest {
 
     Match match;
     Team local, visitor;
-    Player forward;
+    Player forward, defender, goalkeeper, midfield, rivalDefender;
 
     public void makeMatchWithTeams() throws Exception {
         local = new Team("Local", new User("Johnny Bravo"));
         visitor = new Team("Visitor", new User("Bugs Bunny"));
-        forward = new Player("Palermo", Position.forward());
+        forward = new Player("Forward", Position.forward());
+        defender = new Player("Defender", Position.defender());
+        rivalDefender = new Player("DefenderR", Position.defender());
+        goalkeeper = new Player("GK", Position.goalKeeper());
+        midfield = new Player("Midfield", Position.midfield());
         local.addPlayer(forward);
+        local.addPlayer(defender);
+        local.addPlayer(goalkeeper);
+        local.addPlayer(midfield);
+        visitor.addPlayer(rivalDefender);
         match = new Match(1, local, visitor);
     }
 
@@ -26,9 +35,38 @@ public class MatchTest {
     }
 
     @Test
-    public void itShouldAddOneGoalOfAForwardToTheLocalTeam() throws Exception {
+    public void itShouldAddOneGoalOfAForwardToTheLocalTeamAndEqualOnePoint() throws Exception {
+        match.addGoal(visitor, rivalDefender);
         match.addGoal(local, forward);
 
         assertTrue(match.pointsOf(local).equals(1));
+    }
+
+    @Test
+    public void itShouldAddOneGoalOfADefenderToTheLocalTeamAndEqualOnePoint() throws Exception {
+        match.addGoal(visitor, rivalDefender);
+        match.addGoal(local, midfield);
+
+        assertTrue(match.pointsOf(local).equals(1));
+    }
+
+    @Test
+    public void itShouldAddOneGoalOfADefenderToTheLocalTeamAndEqualThreePoints() throws Exception {
+        match.addGoal(visitor, rivalDefender);
+        match.addGoal(local, defender);
+
+        assertTrue(match.pointsOf(local).equals(3));
+    }
+
+    @Test
+    public void itShouldHaveTwoPointsWhenTheGoalKeeperDidNotReceiveAnyGoals() throws Exception {
+        assertTrue(match.pointsOf(local).equals(2));
+    }
+
+    @Test
+    public void itShouldHaveNoPointsForTheGoalKeeperWhenTheRivalScoredAtLeastOneGoal() throws Exception {
+        match.addGoal(visitor, rivalDefender);
+
+        assertFalse(match.pointsOf(local).equals(2));
     }
 }
