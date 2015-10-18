611ed3ab67b8b62d32d11546985814b563a00f21
jcalvento <julian.calvento@10pines.com>
2015-10-18 00:00:22 -0300

    Fix travis config

--- super_goal/spec/factories/factory_girl.rb
+++ super_goal/spec/factories/factory_girl.rb
@@ -1,7 +1,7 @@
-RSpec.configure do |config|
-  # additional factory_girl configuration
-
-  config.before(:suite) do
-    FactoryGirl.lint
-  end
-end
+# RSpec.configure do |config|
+#   # additional factory_girl configuration
+#
+#   config.before(:suite) do
+#     FactoryGirl.lint
+#   end
+# end
