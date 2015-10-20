e84865318cfa3d649b013ecf6b70b9e7b6a9f30a
jcalvento <julian.calvento@10pines.com>
2015-10-20 19:28:08 -0300

    Disable cache

--- super_goal/app/controllers/application_controller.rb
+++ super_goal/app/controllers/application_controller.rb
@@ -3,13 +3,19 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
-  before_filter :cors
+  before_filter :cors, :set_cache_buster
 
   def cors
     headers['Access-Control-Allow-Origin'] = 'http://localhost:9000'
     headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
     headers['Access-Control-Request-Method'] = '*'
-    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cache-Control'
+  end
+
+  def set_cache_buster
+    response.headers['Cache-Control'] = 'no-cache, no-store, max-age=0, must-revalidate'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = 'Fri, 01 Jan 1990 00:00:00 GMT'
   end
 
   def options
