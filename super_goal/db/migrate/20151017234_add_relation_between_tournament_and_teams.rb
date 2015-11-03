3cdb85d7428df37c9c44ba700644c3096e6a5397
patriciotella <patriciotella@gmail.com>
2015-11-03 17:27:54 -0300

    fix migration - relation between teams and tournament

--- super_goal/db/migrate/20151017234_add_relation_between_tournament_and_teams.rb
+++
@@ -1,9 +0,0 @@
-class AddRelationBetweenTournamentAndTeams < ActiveRecord::Migration
-  def up
-    add_belongs_to :teams, :tournament
-  end
-
-  def down
-    remove_belongs_to :teams, :tournament
-  end
-end
--- super_goal/db/schema.rb
+++ super_goal/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151017235055) do
+ActiveRecord::Schema.define(version: 20151103201805) do
 
   create_table "date_matches", force: :cascade do |t|
     t.date     "date"
